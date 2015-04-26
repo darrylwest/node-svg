b9e4358294ad1d717c19ad3d91f731515768113a
darryl west <darryl.west@RainCitySoftware.com>
2015-04-25 18:13:33 -0700

    initial instance tests

---
+++ test/NodeSVGTests.js
@@ -0,0 +1,28 @@
+/**
+ * @class NodeSVGTests
+ *
+ * @author: darryl.west@raincitysoftware.com
+ * @created: 4/25/15 5:57 PM
+ */
+var should = require('chai').should(),
+    jsdom = require('node-jsdom' ).jsdom,
+    NodeSVG = require('../lib/NodeSVG');
+
+describe('MockBrowser', function() {
+    'use strict';
+
+    var document = jsdom('mock' ),
+        window = document.parentWindow;
+
+    global.document = document;
+    global.window = window;
+
+    describe('#instance', function() {
+        it('should create and instance of node svg', function() {
+            var svg; // = new NodeSVG();
+
+            should.exist( document );
+        });
+
+    });
+});
